db8d3a99743b3647e3e738b5260c27bbfc4217a0
bily81 <72961580+bily81@users.noreply.github.com>
2020-12-24 23:04:43 +0700

    Add files via upload

---
+++ Nilai.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NilaiSiswa;
+
+/**
+ *
+ * @author Hp Pavillion 14
+ */
+public class Nilai {
+    
+     public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+
+}
+   
